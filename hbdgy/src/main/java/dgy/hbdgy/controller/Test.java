c9906f6dba0e1bbf50475715fc3c0822eb35fc52
[deng_gang] <[2692909219@qq.com]>
2019-11-30 14:26:33 +0800

    vue后端代码仓库

---
+++ hbdgy/src/main/java/dgy/hbdgy/controller/Test.java
@@ -0,0 +1,5 @@
+package dgy.hbdgy.controller;
+
+public class Test {
+    //这只是一个测试
+}
---
+++ src/main/java/StudetEntity/Student.java
@@ -0,0 +1,7 @@
+package StudetEntity;
+
+public class Student {
+  public static  void main(String arg[]){
+      System.out.println("ok!");
+  }
+}
