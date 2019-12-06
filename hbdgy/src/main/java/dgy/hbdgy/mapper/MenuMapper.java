9daf179aa75992e47b86ff53e66f6ec38b5839bb
[deng_gang] <[2692909219@qq.com]>
2019-12-06 09:21:09 +0800

    2019/12/6

--- hbdgy/src/main/java/dgy/hbdgy/mapper/MenuMapper.java
+++ hbdgy/src/main/java/dgy/hbdgy/mapper/MenuMapper.java
@@ -1,7 +1,10 @@
 package dgy.hbdgy.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import java.util.List;
-
+@Mapper
+@Repository
 public interface MenuMapper {
 
     //获取所有的菜单信息
